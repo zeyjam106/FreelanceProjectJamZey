7d2538cb0711b8ba49a26b3d3ae662109b632ded
zeyjam106 <54830058+zeyjam106@users.noreply.github.com>
2020-03-04 18:53:05 +0400

    terraforming

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+#Created By JamZey
+
+variable "github_token" {
+  description = "GitToken to be used for the CodePipeline"
+  type        = "string"
+}
+
+variable "account_id" {
+  description = "id of the active account"
+  type        = "string"
+}
+
+variable "region" {
+  description = "region to deploy to"
+  type        = "string"
+}
+
+provider "aws" {
+  region  = "${var.region}"
+  version = "2.7"
+}
